c5bb20d9a290d791bbb36c30b809c8af4347bd53
Amr Sharaf <amrmagdy.sharaf@gmail.com>
2018-03-08 02:46:13 -0500

    Added commenting

--- convert.py
+++ convert.py
@@ -13,21 +13,32 @@ def convert_currency(currency, value):
         return currency
     return currency * float(value)
 
+#Parse arguments and provide usage message
 parser=argparse.ArgumentParser(
     description='''Currency converter''')
 parser.add_argument('--field', type=int)
 parser.add_argument('--multiplier', type=float)
 args=parser.parse_args()
+#Extract multiplier argument
 multiplier = args.multiplier
+#Extract field argument
 field = args.field
+#Read the first_line and write it right after without change since it contains titles
 first_line = sys.stdin.readline()
 sys.stdout.write(first_line)
+#Read the rest of the file
 file = sys.stdin.read()
+#Separate the lines of the file into a list
 lines = file.split("\r\n")
+#Go through each line in the file
 for line in lines:
+    #Separate the input using the conventional , for the csv files
     inputs = line.split(",")
+    #Convert currency
     inputs[field - 1] = convert_currency(multiplier, inputs[field - 1])
+    #Concatenate the input again into one line and append the network newline character
     new_line = ','.join(map(str, inputs))
     new_line += "\r\n"
     line = new_line
+    #write the new_line to stdout
     sys.stdout.write(line)
